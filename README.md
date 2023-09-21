dccf8360082db5d1f2710e802fe6b842e2765d98
InshaSiS <136191740+Inshasis@users.noreply.github.com>
2023-09-22 00:12:57 +0530

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+## Magento Integration
+
+Magento 2.0 Two Way Integration With ERPNext
+
+#### License
+
+MIT
---
+++ magento_integration/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Magento Integration",
+			"type": "module",
+			"label": _("Magento Integration")
+		}
+	]
